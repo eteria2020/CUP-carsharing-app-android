815ca3c16fcf091ec6b9fca0329099f0fa7e4d89
Greta Iaconisi <greta.iaconisi@gr3dcomunication.com>
2017-05-19 17:38:56 +0200

    Tootlbar, apertura menu, init Activity Mappa

---
+++ app/src/main/java/it/sharengo/eteria/ui/map/MapFragment.java
@@ -0,0 +1,52 @@
+package it.sharengo.eteria.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import it.sharengo.eteria.R;
+import it.sharengo.eteria.ui.base.fragments.BaseMvpFragment;
+
+
+public class MapFragment extends BaseMvpFragment<MapPresenter> implements MapMvpView {
+
+    private static final String TAG = MapFragment.class.getSimpleName();
+
+    public static MapFragment newInstance() {
+        MapFragment fragment = new MapFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        getMvpFragmentComponent(savedInstanceState).inject(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //                                              ButterKnife
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //                                              Mvp Methods
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+}
