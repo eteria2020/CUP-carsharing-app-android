bd2725f78789d8a309193e1eed0d484265275f4f
Greta Iaconisi <greta.iaconisi@gr3dcomunication.com>
2017-06-18 12:26:50 +0200

    Progress Login

--- app/src/main/java/it/sharengo/development/ui/login/LoginFragment.java
+++ app/src/main/java/it/sharengo/development/ui/login/LoginFragment.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.osmdroid.views.MapView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.sharengo.development.R;
 import it.sharengo.development.ui.base.fragments.BaseMvpFragment;
@@ -13,6 +19,25 @@ import it.sharengo.development.ui.base.fragments.BaseMvpFragment;
 public class LoginFragment extends BaseMvpFragment<LoginPresenter> implements LoginMvpView {
 
     private static final String TAG = LoginFragment.class.getSimpleName();
+
+    @BindView(R.id.emailEditText)
+    EditText emailEditText;
+
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+
+    @BindView(R.id.forgotButton)
+    TextView forgotButton;
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
+
+    @BindView(R.id.registerButton)
+    Button registerButton;
+
+    @BindView(R.id.continueButton)
+    TextView continueButton;
+
 
     public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
