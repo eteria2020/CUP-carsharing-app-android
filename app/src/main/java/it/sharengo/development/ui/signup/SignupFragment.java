2cf7828373ca9d861456f26eba05a0c54616b4fa
Greta Iaconisi <greta.iaconisi@gr3dcomunication.com>
2017-06-20 23:35:51 +0200

    Completed signup

--- app/src/main/java/it/sharengo/development/ui/signup/SignupFragment.java
+++ app/src/main/java/it/sharengo/development/ui/signup/SignupFragment.java
@@ -1,18 +1,32 @@
 package it.sharengo.development.ui.signup;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.sharengo.development.R;
+import it.sharengo.development.routing.Navigator;
 import it.sharengo.development.ui.base.fragments.BaseMvpFragment;
+import it.sharengo.development.ui.passwordrecovery.PasswordRecoveryFragment;
 
 
 public class SignupFragment extends BaseMvpFragment<SignupPresenter> implements SignupMvpView {
 
     private static final String TAG = SignupFragment.class.getSimpleName();
+
+    @BindView(R.id.signupWebView)
+    WebView webview;
 
     public static SignupFragment newInstance() {
         SignupFragment fragment = new SignupFragment();
@@ -31,9 +45,65 @@ public class SignupFragment extends BaseMvpFragment<SignupPresenter> implements 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
         mUnbinder = ButterKnife.bind(this, view);
+
+        loadWebView();
+
         return view;
     }
 
+    private void loadWebView(){
+
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                getActivity().setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                showError(getString(R.string.error_generic_msg));
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.equals("http://www.sharengo.it/signup-3/mobile")) {
+                    loadLogin();
+                    return true;
+                }
+                return false;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url=request.getUrl().toString();
+                if (url.equals("http://www.sharengo.it/signup-3/mobile")) {
+                    loadLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        webview.loadUrl("http://www.sharengo.it/signup/mobile");
+
+    }
+
+    private void loadLogin(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Navigator.launchLogin(SignupFragment.this, Navigator.REQUEST_LOGIN_START);
+                getActivity().finish();
+            }
+        }, 3000);
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //
