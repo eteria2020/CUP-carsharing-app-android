851ba59d8924bff713615adda5fcf7a44d50ffba
Greta Iaconisi <greta.iaconisi@gr3dcomunication.com>
2017-08-20 19:52:51 +0200

    Progress loader animation

--- app/src/main/java/it/sharengo/development/ui/base/activities/BaseActivity.java
+++ app/src/main/java/it/sharengo/development/ui/base/activities/BaseActivity.java
@@ -1,9 +1,12 @@
 package it.sharengo.development.ui.base.activities;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import it.handroix.core.base.HdxBaseActivity;
 import it.sharengo.development.R;
@@ -23,12 +26,21 @@ public abstract class BaseActivity extends HdxBaseActivity {
     public void showLoading() {
         Log.w("SHOW","LOADING");
         findViewById(R.id.customLoading).setVisibility(View.VISIBLE);
+        ImageView customBkgLoading = (ImageView)findViewById(R.id.customBkgLoading);
+        customBkgLoading.setBackgroundResource(R.drawable.loader_bkg_animation);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) customBkgLoading.getBackground();
+        frameAnimation.start();
+
+        checkIfAnimationDone(frameAnimation);
+
         //super.showLoading();
     }
 
     public void hideLoading() {
         //super.hideLoading();
-        findViewById(R.id.customLoading).setVisibility(View.GONE);
+        //findViewById(R.id.customLoading).setVisibility(View.GONE);
     }
 
     public void replaceFragment(Fragment fragment) {
@@ -36,4 +48,19 @@ public abstract class BaseActivity extends HdxBaseActivity {
                 .replace(R.id.content_frame, fragment)
                 .commit();
     }
+
+    private void checkIfAnimationDone(AnimationDrawable anim){
+        final AnimationDrawable a = anim;
+        int timeBetweenChecks = 300;
+        Handler h = new Handler();
+        h.postDelayed(new Runnable(){
+            public void run(){
+                if (a.getCurrent() != a.getFrame(a.getNumberOfFrames() - 1)){
+                    checkIfAnimationDone(a);
+                } else{
+                    findViewById(R.id.customLoading).setVisibility(View.GONE);
+                }
+            }
+        }, timeBetweenChecks);
+    };
 }
