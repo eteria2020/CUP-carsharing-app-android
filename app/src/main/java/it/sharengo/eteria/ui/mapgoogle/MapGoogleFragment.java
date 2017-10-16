772f2259f03704c00238d110a72e869d704eb283
Greta Iaconisi <greta.iaconisi@gr3dcomunication.com>
2017-10-16 22:50:54 +0200

    Bugfix

--- app/src/main/java/it/sharengo/eteria/ui/mapgoogle/MapGoogleFragment.java
+++ app/src/main/java/it/sharengo/eteria/ui/mapgoogle/MapGoogleFragment.java
@@ -742,8 +742,8 @@ public class MapGoogleFragment extends BaseMapFragment<MapGooglePresenter> imple
         userLocation = location;
 
         //TODO Coor
-        userLocation.setLatitude(41.887661);
-        userLocation.setLongitude(12.494472); //Milano 45.510349, 9.093254 - Milano 2 45.464116, 9.191425 - Roma 41.895514, 12.486259    Vinovo 44.975330, 7.617876
+        //userLocation.setLatitude(41.887661);
+        //userLocation.setLongitude(12.494472); //Milano 45.510349, 9.093254 - Milano 2 45.464116, 9.191425 - Roma 41.895514, 12.486259    Vinovo 44.975330, 7.617876
 
         enabledCenterMap(true);
 
@@ -2253,7 +2253,7 @@ public class MapGoogleFragment extends BaseMapFragment<MapGooglePresenter> imple
             if(carToOpen != null){
                 if(userLocation != null){
                     //Calcolo la distanza
-                    if(getDistance(carToOpen) <= 500000000){ //TODO: valore a 50
+                    if(getDistance(carToOpen) <= 50){ //TODO: valore a 50
 
                         //Procediamo con le schermate successive
                         onClosePopup();
